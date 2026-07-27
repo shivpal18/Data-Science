8e71e333ccd9d14791ea35fe056d413c422e6e72
shivpal18 <chaurasiysdilip9794@gmail.com>
2026-07-27 08:57:30 +0530

    update

---
+++ Python/loops.py
@@ -0,0 +1,42 @@
+# For loop
+
+# for i in range(1,21,1):
+    # print(i)
+
+
+# print a table
+# n = int(input("Which table you want ?"))
+# for i in range(n,(n*10)+1,n):
+    # print(i)
+
+
+# loop in string
+# a = "SHIVPAL CHAURASIYA"
+# for i in range(len(a)):
+#     print(a[i])
+
+
+# break statement in for loop
+# for i in range(1,21):
+#     if i ==15:
+#         break
+#     print(i)
+
+
+# continue statement in for loop
+# for i in range(1,21):
+#     if i ==15:
+#         continue
+#     print(i)
+
+
+# else statement in for loop
+for i in range(1,21):
+    if i ==156:
+        print("break statement is exucuted")
+        break
+    print(i)
+else:
+    print("break statement is not executed")
+
+
